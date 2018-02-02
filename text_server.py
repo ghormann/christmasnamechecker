8581e5fed6f13590959c0844baf65fac59dd154e
Greg Hormann <ghormann@thehormanns.net>
2018-02-02 21:59:58 +0000

    Added Status Endpoint

--- text_server.py
+++ text_server.py
@@ -7,12 +7,17 @@ from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 from name_validator import NameValidator
 from mqtt import MQTTClient
+import datetime
 
 mqtt = MQTTClient()
 validator = NameValidator("data/all_names.txt")
 log_file = open("logs/text.log", "a")
 
 app = Flask(__name__)
+
+@app.route("/status", methods=['GET', 'POST'])
+def status_reply():
+    return str("Running")
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
@@ -24,21 +29,22 @@ def sms_reply():
     fromZip = request.values['FromZip']
     fromNum = request.values['From']
     isValid = validator.isValid(textIn)
+    ts = datetime.datetime.now().strftime("%d-%B-%Y %I:%M%p")
    
-    msg=str(isValid) + "|" + fromCity + "|" + fromState + "|" + fromCountry
+    msg= ts + "|" + str(isValid) + "|" + fromCity + "|" + fromState + "|" + fromCountry
     msg += "|" + fromZip + "|" + fromNum + "|" + textIn 
     
     log_file.write(msg)
     log_file.write("\n")
     log_file.flush()
 
-    msg = "The provided name \"" + textIn + "\" wasn't found in our database of 96,000"
-    msg += " names.   I'm afraid it won't be displayed tonight.  I've flagged it to be "
-    msg += " reviewed. If approved, it will be added to the database in the next few days."
+    msg = "The name \"" + textIn + "\" wasn't found in our database of pre-approved names so "
+    msg += "I'm afraid it won't be displayed tonight.  The name has been submitted for human review. "
+    msg += "If approved, it will be added to the database in the next few days."
 
     if isValid:
         mqtt.publishName(textIn)
-        msg = "Thanks! Your name should display soon." 
+        msg = "Thanks " + textIn +  "! Your name should display soon." 
 
     # Start our TwiML response
     resp = MessagingResponse()
