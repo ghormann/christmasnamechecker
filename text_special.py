b301805e3f2e04fa6d3e68f32b7a869c17793c07
Greg Hormann <ghormann@thehormanns.net>
2018-12-09 01:32:39 +0000

    Added way to text back when needed

---
+++ text_special.py
@@ -0,0 +1,21 @@
+#
+# This is the main program for the server and receives 
+# text messages from twilio and sends them to  the
+# home network
+
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+import json
+import sys
+
+config = json.load(open('greglights_config.json'))
+client = Client(config["account_sid"], config["auth_token"]) 
+
+def notifyAdmin(message):
+    message = client.messages.create(
+        to="+15132887558",
+        from_=config["fromPhone"],
+        body=message)
+
+if __name__ == "__main__":
+    notifyAdmin("Izzy is welcome. In a few days she can do the same on the big grid.")
